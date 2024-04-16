028170909f3e82b99d81c4fda6d9827862bc551e
Trinity <thutson3876@gmail.com>
2024-04-16 18:44:09 -0500

    Fixed Fragmenter

--- Assets/Scripts/Destructable.cs
+++ Assets/Scripts/Destructable.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Destructable : MonoBehaviour
 {
+    //public static event EventHandler<FractureEventArgs> fractured;
+
     public void Awake()
     {
         Fragmenter.fractured += fn;
@@ -20,3 +22,13 @@ public class Destructable : MonoBehaviour
         Destroy(obj);
     }
 }
+
+/*public class FractureEventArgs
+{
+    public GameObject obj;
+
+    public FractureEventArgs(GameObject obj)
+    {
+        this.obj = obj;
+    }
+}*/
