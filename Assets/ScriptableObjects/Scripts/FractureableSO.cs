a91a1242bbecd8e68564e26b03f78baea199857e
Trinity <thutson3876@gmail.com>
2024-04-18 13:07:45 -0500

    Assembly Ref Changes (WIP)

--- Assets/ScriptableObjects/Scripts/FractureableSO.cs
+++ Assets/ScriptableObjects/Scripts/FractureableSO.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu]
 public class FractureableSO : ScriptableObject
 {
     [Header("Custom Fracture Options")]
